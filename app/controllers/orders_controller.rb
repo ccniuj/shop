258a6601e62684479ee760052597d9b433118b7e
Juin Chiu <davidjuin0519@gmail.com>
2014-10-13 20:40:35 +0800

    orders#create_contact & remove paid products from cart

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -17,13 +17,6 @@ class OrdersController < ApplicationController
   end
 
   def create
-    # @order = current_user.orders.build(
-    #   contact_id: current_user.contacts.first.id,
-    #   status: 1,
-    #   total_price: params[:total_price],
-    #   pay_method: 1,
-    #   ship_method: 1 )
-
     if params[:contact_id].empty?
       create_contact
       @@contact_id = current_user.contacts.last.id
@@ -57,9 +50,8 @@ class OrdersController < ApplicationController
       par[:amount] = params[:amount][count]
       @item = OrderInventory.new(par)
       @item.save!
+      @cart.remove!(i)
       count += 1
-      #@order.add!(i)
-      #@cart.remove!(p)
     end
   end
 
@@ -68,12 +60,14 @@ class OrdersController < ApplicationController
       @contact = current_user.contacts.new(contact_params)
       @contact.save!
     else
-      User.create!({:email => params[:order][:contact][:email], 
-        :password => params[:order][:contact][:email], 
+      @new_user = User.create!({:email => params[:order][:contact][:email], 
+        :password => params[:order][:contact][:password], 
         :password_confirmation => params[:order][:contact][:password_conformation] })
-
+      Cart.create!({:user_id => @new_user.id})
+      sign_in(@new_user)
+      @contact = current_user.contacts.new(contact_params)
+      @contact.save!
     end
-    
   end
   
   def update_contact
